5307b60b3165ccbd2efadd2be735b0aa8f8832fa
Gregory Daniels <danielslorenzoz2011@gmail.com>
2015-08-04 16:15:58 -0400

    Created UserDAO test...not yet implemented

---
+++ LifeScore/src/ludum/vita/beans/DemographicsBean.java
@@ -0,0 +1,51 @@
+package ludum.vita.beans;
+
+public class DemographicsBean {
+	
+	private String LSUID;
+
+	private String dateOfBirth;
+	
+	private String gender;
+	
+	public DemographicsBean(String gender, String dateOfBirth){
+		this.setGender(gender);
+		this.setDateOfBirth(dateOfBirth);
+	}
+	
+	public DemographicsBean(String LSUID, String gender, String dateOfBirth){
+		this(gender,dateOfBirth);
+		this.setLSUID(LSUID);
+	}
+
+	/**
+	 * @return the lSUID
+	 */
+	public String getLSUID() {
+		return LSUID;
+	}
+
+	/**
+	 * @param lSUID the lSUID to set
+	 */
+	public void setLSUID(String lSUID) {
+		LSUID = lSUID;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+}
---
+++ LifeScore/test/ludum/vita/daotest/UserDAOTest.java
@@ -0,0 +1,24 @@
+package ludum.vita.daotest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserDAOTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
